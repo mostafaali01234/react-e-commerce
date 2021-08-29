cc822d72f389221c059fba89febeff87df2492cc
mostafaali0123 <mostafa.salem.se@gmail.com>
2021-08-29 23:55:12 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Comfy Sloth</header>
+      <div>Store</div>
+      <footer>© 2021 ComfySloth All rights reserved</footer>
     </div>
   );
 }
