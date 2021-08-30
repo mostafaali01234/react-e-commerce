a910da5fa97a33cdf68de42cca3e7fde726e08a7
mostafaali0123 <mostafa.salem.se@gmail.com>
2021-08-31 00:12:58 +0200

    navbar without responsive

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "../img/logo.svg";
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
+import "../css/Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="" />
+        </div>
+
+        <ul className="nav-links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Product</a>
+          </li>
+        </ul>
+        <div>
+          <a className="cart-btn">
+            Cart
+            <span className="cart-container">
+              <FaShoppingCart />
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
